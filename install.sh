1f0995aae689a1efeb73514fd21378ad4e7f649d
Jonas Tranberg <jonastranberg93@gmail.com>
2024-09-18 21:57:24 +0200

    add enabling of extension in install script

--- install.sh
+++ install.sh
@@ -7,5 +7,8 @@ mkdir -p ~/.local/share/gnome-shell/extensions/cloudflare-warp@naueramant.github
 # Copy extension source to the directory
 cp -a "src/." ~/.local/share/gnome-shell/extensions/cloudflare-warp@naueramant.github.com
 
+# Enble the extension
+gnome-extensions enable cloudflare-warp@naueramant.github.com
+
 # Inform the user to restart GNOME Shell
 echo "Please restart GNOME Shell by pressing Alt + F2 and typing 'r' and pressing Enter"
